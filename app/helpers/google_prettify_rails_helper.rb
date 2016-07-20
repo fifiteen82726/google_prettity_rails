122d43a8f851c5cca1e7452c2f9bab93eb5aa6f6
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-20 10:23:13 +0800

    add engine

---
+++ app/helpers/google_prettify_rails_helper.rb
@@ -0,0 +1,7 @@
+module GooglePrettifyRails
+    def self.put_code
+		# content_tag(:pre, a, class: "prettyprint")
+		content_tag(:p, "hello")
+	end
+
+end
--- lib/google_prettify_rails.rb
+++ lib/google_prettify_rails.rb
@@ -1,8 +1,10 @@
 require "google_prettify_rails/version"
 
 module GooglePrettifyRails
-    def self.put_code(a)
-		content_tag(:pre, a, class: "prettyprint")
-	end
+	class Engine < ::Rails::Engine
+ #    def self.put_code
+	# 	# content_tag(:pre, a, class: "prettyprint")
+	# 	content_tag(:p, "hello")
+	# end
 
 end
