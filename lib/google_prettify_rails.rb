303a0f3afaf0f7019bc014d4fe29c1ea7a4ebaf5
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-20 11:24:57 +0800

    modify bug

--- lib/google_prettify_rails.rb
+++ lib/google_prettify_rails.rb
@@ -2,10 +2,6 @@ require "google_prettify_rails/version"
 
 module GooglePrettifyRails
 	class Engine < ::Rails::Engine
- #    def self.put_code
-	# 	# content_tag(:pre, a, class: "prettyprint")
-	# 	content_tag(:p, "hello")
-	# end
  	end
 
 end
